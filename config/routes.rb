c971a6d5ef79340f062f73feee1e7da3fc1d6646
ohataken <11092425+ohataken@users.noreply.github.com>
2023-10-02 23:12:48 +0900

    update sessions controller

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,9 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+  resource :session, only: [:new], path: "oauth2" do
+    get :callback, on: :collection
+  end
+
   root "pages#root"
 end
