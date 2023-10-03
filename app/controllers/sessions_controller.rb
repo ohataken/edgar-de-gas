caf1bce8af5d525e94344f2433d003e4e52fe67b
ohataken <11092425+ohataken@users.noreply.github.com>
2023-10-03 22:48:24 +0900

    add ApplicationToken

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,52 +1,12 @@
 class SessionsController < ApplicationController
-  helper_method :token_key
-
   def new
-    redirect_to authorizer.get_authorization_url(request: request), allow_other_host: true
+    @token = ApplicationToken.new
+    redirect_to @token.authorizer.get_authorization_url(request: request), allow_other_host: true
   end
 
   def callback
-    authorizer.handle_auth_callback(token_key, request)
-  end
-
-  private
-
-  def client
-    @client ||= create_client
-  end
-
-  def create_client
-    client_id = ENV.fetch("GOOGLE_CLIENT_ID", "xxxx.apps.googleusercontent.com")
-    client_secret = ENV.fetch("GOOGLE_CLIENT_SECRET", "xxxxxxxx")
-    Google::Auth::ClientId.new(client_id, client_secret)
-  end
-
-  def token_store
-    @token_store ||= create_token_store
-  end
-
-  def create_token_store
-    Google::Auth::Stores::RedisTokenStore.new(redis: Redis::Objects.redis)
-  end
-
-  def authorizer
-    @authorizer ||= create_authorizer
-  end
-
-  def create_authorizer
-    Google::Auth::WebUserAuthorizer.new(client, oauth2_scope, token_store, '/oauth2/callback')
-  end
-
-  def oauth2_scope
-    [
-      'https://www.googleapis.com/auth/drive',
-      'https://www.googleapis.com/auth/script.processes',
-      'https://www.googleapis.com/auth/script.projects',
-      'https://www.googleapis.com/auth/script.deployments',
-    ]
-  end
-
-  def token_key
-    @token_key ||= SecureRandom.alphanumeric
+    @token = ApplicationToken.new
+    @token.generate_key
+    @token.authorizer.handle_auth_callback(@token.key, request)
   end
 end
---
+++ app/models/application_token.rb
@@ -0,0 +1,58 @@
+class ApplicationToken
+  attr_reader :key, :token
+
+  def initialize key = ""
+    @key = key
+  end
+
+  def client
+    @client ||= create_client
+  end
+
+  def create_client
+    client_id = ENV.fetch("GOOGLE_CLIENT_ID", "xxxx.apps.googleusercontent.com")
+    client_secret = ENV.fetch("GOOGLE_CLIENT_SECRET", "xxxxxxxx")
+    Google::Auth::ClientId.new(client_id, client_secret)
+  end
+
+  def token_store
+    @token_store ||= create_token_store
+  end
+
+  def create_token_store
+    Google::Auth::Stores::RedisTokenStore.new(redis: Redis::Objects.redis)
+  end
+
+  def authorizer
+    @authorizer ||= create_authorizer
+  end
+
+  def create_authorizer
+    Google::Auth::WebUserAuthorizer.new(client, oauth2_scope, token_store, '/oauth2/callback')
+  end
+
+  def oauth2_scope
+    [
+      'https://www.googleapis.com/auth/drive',
+      'https://www.googleapis.com/auth/script.processes',
+      'https://www.googleapis.com/auth/script.projects',
+      'https://www.googleapis.com/auth/script.deployments',
+    ]
+  end
+
+  def generate_key
+    @key = SecureRandom.alphanumeric
+  end
+
+  def load
+    @token = token_store.load(@key)
+  end
+
+  def store
+    token_store.store(@key, @token)
+  end
+
+  def delete
+    token_store.delete(@key)
+  end
+end
