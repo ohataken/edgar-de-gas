0976cb28c6ef409f86c019eceabb793887f332f3
ohataken <11092425+ohataken@users.noreply.github.com>
2023-10-04 00:12:46 +0900

    rails generate controller api/project_deployments

---
+++ app/controllers/api/project_deployments_controller.rb
@@ -0,0 +1,2 @@
+class Api::ProjectDeploymentsController < ApplicationController
+end
---
+++ test/controllers/api/project_deployments_controller_test.rb
@@ -0,0 +1,7 @@
+require "test_helper"
+
+class Api::ProjectDeploymentsControllerTest < ActionDispatch::IntegrationTest
+  # test "the truth" do
+  #   assert true
+  # end
+end
